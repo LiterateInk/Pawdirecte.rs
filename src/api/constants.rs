e8cf5ca398be1906e54896e2e4d2059ac95a993a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-26 17:59:13 +0200

    feat: add API constants

---
+++ src/api/constants.rs
@@ -0,0 +1,5 @@
+/// API only for staff and teachers.
+pub const APIP_URL: &str = "https://apip.ecoledirecte.com";
+
+/// API for everyone else - students, parents, ...
+pub const API_URL: &str = "https://api.ecoledirecte.com";
