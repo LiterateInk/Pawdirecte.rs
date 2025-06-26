4a4a80e37c338a2df2ba866b43415acb29369484
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-26 17:59:58 +0200

    feat(def): add class models

---
+++ src/definitions/models/class.rs
@@ -0,0 +1,11 @@
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Class {
+    pub id: u32,
+    pub code: String,
+    #[serde(rename = "libelle")]
+    pub label: String,
+    #[serde(rename = "estNote")]
+    pub is_graded: u32,
+}
