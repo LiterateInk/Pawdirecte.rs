dbdf84fa1394e730dda924625d44eece04b54671
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-07-08 00:28:19 +0200

    feat(api): add version and user-agent constants

--- src/api/constants.rs
+++ src/api/constants.rs
@@ -1,5 +1,10 @@
+use const_format::formatcp;
+
 /// API only for staff and teachers.
 pub const APIP_URL: &str = "https://apip.ecoledirecte.com";
 
 /// API for everyone else - students, parents, ...
 pub const API_URL: &str = "https://api.ecoledirecte.com";
+
+pub const API_VERSION: &str = "7.0.1";
+pub const USER_AGENT: &str = formatcp!("Android EDMOBILE v{API_VERSION}");
