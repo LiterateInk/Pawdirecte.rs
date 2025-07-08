cd9462a20360685104d884daac8d5f5a221012c0
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-07-08 22:22:17 +0200

    chore: remove old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-// pub struct AuthenticationDoubleAuth {
-//     cn: String,
-//     cv: String,
-// }
-
-// pub struct Authentication {
-//     username: String,
-//     password: String,
-//     device_uuid: String,
-//     double_auth: Option<AuthenticationDoubleAuth>,
-// }
-
-// impl Authentication {}
-
 pub mod api;
 pub mod definitions;
