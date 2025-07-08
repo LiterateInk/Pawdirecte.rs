2041d4658fc44121b1d940da9f77e1479798731b
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-07-08 22:23:22 +0200

    chore: bump app version

--- src/api/constants.rs
+++ src/api/constants.rs
@@ -6,5 +6,5 @@ pub const APIP_URL: &str = "https://apip.ecoledirecte.com";
 /// API for everyone else - students, parents, ...
 pub const API_URL: &str = "https://api.ecoledirecte.com";
 
-pub const API_VERSION: &str = "7.0.1";
+pub const API_VERSION: &str = "7.2.3";
 pub const USER_AGENT: &str = formatcp!("Android EDMOBILE v{API_VERSION}");
