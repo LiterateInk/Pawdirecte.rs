47f7d7fd2f003ddbc1fcbc6c5e13ab6792164d76
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-07-08 22:23:36 +0200

    fix(api): add transparent errors from reqwest, cookie parser and serde

--- src/api/errors.rs
+++ src/api/errors.rs
@@ -2,4 +2,12 @@
 pub enum Error {
   #[error("unauthenticated session")]
   WrongLoginState(),
+  #[error("GTK cookie not found, is EcoleDirecte up?")]
+  CookieGtkNotFound(),
+  #[error(transparent)]
+  Network(#[from] reqwest::Error),
+  #[error(transparent)]
+  Cookie(#[from] cookie_parser::CookieParseError),
+  #[error(transparent)]
+  Json(#[from] serde_json::Error),
 }
