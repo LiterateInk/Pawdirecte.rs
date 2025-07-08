336ff80a74a7397b71eb47ccb20bfcc2bf52c0dc
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-07-08 22:24:18 +0200

    feat(api): wip on init of login manager

--- src/api/login_manager.rs
+++ src/api/login_manager.rs
@@ -1,6 +1,18 @@
+use std::{collections::HashMap, str::FromStr};
+
 use crate::{
-  api::{Authentication, Error, RequestManager},
-  definitions::{models::Account, responses::LoginResponse},
+  api::{
+    API_URL, API_VERSION, Authentication, Error, RequestManager, USER_AGENT,
+  },
+  definitions::{
+    models::Account, requests::LoginRequest, responses::LoginResponse,
+  },
+};
+
+use cookie_parser::{CookiePair, SetCookie, parse_set_cookie};
+use reqwest::{
+  Client,
+  header::{self, HeaderName, SET_COOKIE},
 };
 
 /// Will handle most of the authentication.
@@ -44,7 +56,73 @@ impl LoginManager {
   ///
   /// See `finalize` for retrieving accounts.
   pub async fn initialize(&self) -> Result<(), Error> {
-    Ok(())
+    let mut gtk: Option<String> = None;
+    let mut cookies: Vec<CookiePair> = vec![];
+
+    let url = format!("{API_URL}/v3/login.awp?gtk=1&v={API_VERSION}");
+
+    let client = Client::new();
+    let response = client
+      .get(&url)
+      .header(header::USER_AGENT, USER_AGENT)
+      .send()
+      .await?;
+
+    for cookie in response.headers().get_all(SET_COOKIE).iter() {
+      let cookie = parse_set_cookie(cookie.to_str().unwrap())?;
+
+      if cookie.pair.name == "GTK" {
+        gtk = Some(cookie.pair.value.clone());
+      }
+
+      cookies.push(cookie.pair);
+    }
+
+    if let Some(gtk) = gtk {
+      let cookies = cookies
+        .iter()
+        .map(|pair| format!("{}={}", pair.name, pair.value))
+        .collect::<Vec<String>>()
+        .join("; ");
+
+      let mut params = HashMap::new();
+
+      // We're not already authenticated, let's run the initial procedure.
+      if self.authentication.access_token.is_none() {
+        let auth = self.authentication.clone();
+
+        let json = LoginRequest {
+          device_uuid: auth.device_uuid,
+          is_reauth: false,
+          password: auth.password.trim().into(),
+          remember_me: Some(true),
+          username: auth.username,
+          access_token: None,
+          account_type: None,
+          double_auth: None,
+        };
+
+        let json = serde_json::to_string(&json)?;
+        params.insert("data", json);
+      }
+
+      let url = format!("{API_URL}/v3/login.awp?v={API_VERSION}");
+      let response = client
+        .post(&url)
+        .header("X-GTK", &gtk)
+        .header(header::COOKIE, &cookies)
+        .header(header::USER_AGENT, USER_AGENT)
+        .form(&params)
+        .send()
+        .await?;
+
+      let text = response.text().await?;
+      println!("{text}");
+
+      Ok(())
+    } else {
+      Err(Error::CookieGtkNotFound())
+    }
   }
 
   pub fn finalize(&self) -> Result<Vec<Account>, Error> {
